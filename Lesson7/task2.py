f775bc6d2546ccb2679216a2fcc9e6c9d055aaff
satikpro <saturnpro73@gmail.com>
2022-01-22 13:21:35 +0200

    first commit

---
+++ Lesson7/task2.py
@@ -0,0 +1,11 @@
+#F = ["ä", "b", "c"]
+#for i in F:
+#    print(i, end = " ")
+
+#n=int(input("n= "))
+
+#for item in range(1, n+1):
+ #   print(item)
+n = 0,5
+for i in range(2,7,n):
+        print(f"{i}")
